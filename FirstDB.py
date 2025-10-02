413a65d8202ef9db97dd849bec67b4b8aec3bf82
“Danell <danelljhamilton@gmail.com>
2025-10-02 09:58:28 -0600

    fixer

--- FirstDB.py
+++ FirstDB.py
@@ -12,94 +12,80 @@ with conn, conn.cursor() as cur:
     print(cur.fetchone())
 
 
-def showLocations():
-    
-    print( "     x|      y|      id")
-    print("---------------------")
+def showLocations(data):
+    print("\t id|\t x|\t y|")
+    print("------------------------------------")
     for row in data:
         for value in row:
-            print(value,"|",end="")
-            print("|", end = "")
-            print("")
+            print("\t",value,"|",end="")
+        print("")
+    print("------------------------------------")
 
-def getLocation():
-    cursor.execute("Select * from location")
-    data = cursor.fecthall()
-    print(data)
+def getLocations(): # R in CRUD
+    cursor.execute("Select id,x,y from location")
+    data=cursor.fetchall()
+#    print(data)
     return data
 
 def putLocation(values):
-    first ="update location set "
-    last =f"where id = {d ["id"] }"
-    value1 ="" 
+    cursor.execute(f"insert into location (x,y) values ({values[0]},{values[1]})")
+
+
+def getLocationFromUser():  # C in CRUD 
+    x=input("Please enter X:")
+    y=input("Please enter Y:")
+    return (x,y)
+
+def putChange(d): # d is a dictinary that include id and either x, y or both
+    first="update location set "
+    id=d["id"]
+    last=f" where id={id}"
+    value1=""
     if "x" in d:
-        value =f"x={3}"
-        value2=""
+        x=d["x"]
+        value1=f"x={x}"
+    value2=""
     if "y" in d:
+        y=d["y"]
         value2=f"y={y}"
-        if"x" in d and "y" in d:
-            value1 = value1+","
-        cursor.execute(first+value1+value2+last)
+    if ("x" in d) and ("y" in d):
+        value1=value1+","
+    cursor.execute(first+value1+value2+last)
 
-def addLocation():
-    x = input("Please enter X")
-    y = input("Please enter y")
-    cursor.execute(f"insert into location (x,y) values ({x}, {y} )")
+def updateLocation():
+    choice=0
+    print("0) Cancel")
+    print("1) Change X")
+    print("2) Change Y")
+    print("3) Change Both")
+    choice=int(input("Enter your choice to change a parameter"))
+    if choice!=0:
+        id=input("Enter id you want to update:")
+        if choice==1 or choice==3:
+            x=input("Enter new X:")
+        if choice==2 or choice==3:
+            y=input("Enter new Y:")
+        if choice==1: 
+            putChange( {"id":id,"x":x} )
+        if choice==2:
+            putChange( {"id":id,"y":y} )
+        if choice==3:
+            putChange( {"id":id,"x":x,"y":y})
 
-def putChange(values):
-
-    def updateLocation():
-        # id = input("Enter id you want to update:")
-        # x = input('Enter new X:")' \
-        # 'y=input("Enter new Y:") ')
-        choice =0
-        print("0) Cancel")
-        print("1) Change X")
-        print("2) Change Y")
-        print("3) Change Both")
-        choice = input("Enter your choice to change a parameter")
-        if choice !=0:
-            id = input("Enter id you want to update:")
-            if choice==1:
-                    x=input("Enter new X:")
-                    cursor.execute(f"update location set x = (x) where id = (id)")
-            if choice==2:
-                y=input("Enter new Y:")
-                cursor.execute(f"update location set y = (y) where id =(id)")
-                if choice ==3:
-                    x=input("Enter new X:")
-                    y = input("Enter new Y:)")
-                if choice ==1: 
-                    putchange((id,x))
-                if choice ==2:
-                    putchange((id,y))
-                if choice == 3:
-                    putchange((id,x,y))
-            
-
-        choice = 1
-        while choice!=0:
-            print("0) Quit")
-            print("1) Show Location")
-            print("2 ) Add locstion")
-            print("3) Update Locatino U")
-            print("4) Delete (to be completed by student)")
-            print("5) commit current changes")
-            choice = int(input("Enter your choice"))
-            if choice == 1:
-                showLocations(getLocation())
-            if choice==2:
-                putLocation(getLocation())
-            if choice==3:
-                updateLocation()
-            if choice ==5:
-                connection.commit()
-        
-
-
-            cursor.execute("Select * from common")
-
-    data=cursor.fetchall()
-
-    print(data)
-
+choice=1
+while choice!=0:
+    print("0) Quit")
+    print("1) Show Locations R")
+    print("2) Add Location C")
+    print("3) Update Location U")
+    print("4) Delete (to be completed by student)")
+    print("5) Commit current changes")
+    choice=int(input("Enter your choice: "))
+    if choice==1:
+        showLocations(getLocations())
+    if choice==2:
+        putLocation(getLocationFromUser())
+    if choice==3:
+        updateLocation()
+    if choice==5:
+        connection.commit()
